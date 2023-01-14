e83a476d85f7ffdd0a49104935239fa723b44fa2
s3chkin <seckins191@gmail.com>
2023-01-14 14:15:30 +0200

    updated

---
+++ MVC-Freelancer/Controllers/PortfolioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Freelancer.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Portfolio() 
+        { 
+            return View();
+        }
+    }
+}
